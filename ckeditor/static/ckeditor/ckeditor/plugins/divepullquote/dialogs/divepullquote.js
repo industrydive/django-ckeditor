390312e49ebcc235892c8927181c5ca495032521
Hayden Wagner <hwagner@industrydive.com>
2016-08-04 12:20:18 -0400

    Adds dialog functions to construct new pullquote elements, renames icon png

--- ckeditor/static/ckeditor/ckeditor/plugins/divepullquote/dialogs/divepullquote.js
+++ ckeditor/static/ckeditor/ckeditor/plugins/divepullquote/dialogs/divepullquote.js
@@ -30,48 +30,68 @@ CKEDITOR.dialog.add( 'divepullquoteDialog', function ( editor ) {
 		                label: 'Explanation',
 		                validate: CKEDITOR.dialog.validate.notEmpty( "Explanation field cannot be empty." ),
 		                setup: function( element ) {
-		                	this.setValue( element.getAttribute( "title" ) );
+		                	this.setValue( element.getAttribute( "class" ) );
 		                },
 		                commit: function( element ) {
-		                	element.setAttribute( "title", this.getValue() );
+		                	element.setAttribute( "class", this.getValue() );
 		                }
 		            }
 		        ]
 		    },
-		    {
-		        id: 'tab-adv',
-		        label: 'Advanced Settings',
-		        elements: [
-		            {
-		                type: 'text',
-		                id: 'id',
-		                label: 'Id',
-		                setup: function( element ) {
-		                	this.setValue( element.getAttribute( "id" ) );
-		                },
-		                commit: function( element ) {
-		                	var id = this.getValue();
-
-		                	if( id )
-		                		element.setAttribute( 'id', id );
-		                	else if( !this.insertMode )
-		                		element.removeAttribute( 'id' );
-		                }
-		            }
-		        ]
-		    }
+		    // {
+		    //     id: 'tab-adv',
+		    //     label: 'Advanced Settings',
+		    //     elements: [
+		    //         {
+		    //             type: 'text',
+		    //             id: 'id',
+		    //             label: 'Id',
+		    //             setup: function( element ) {
+		    //             	this.setValue( element.getAttribute( "id" ) );
+		    //             },
+		    //             commit: function( element ) {
+		    //             	var id = this.getValue();
+            //
+		    //             	if( id )
+		    //             		element.setAttribute( 'id', id );
+		    //             	else if( !this.insertMode )
+		    //             		element.removeAttribute( 'id' );
+		    //             }
+		    //         }
+		    //     ]
+		    // }
 		],
 
         onShow: function() {
             var selection = editor.getSelection();
             var element = selection.getStartElement();
+			console.log(element);
 
             if ( element )
-                element = element.getAscendant( 'abbr', true );
+                //CHANGE OLD
+            	//element = element.getAscendant( 'abbr', true );
 
-            if ( !element || element.getName() != 'abbr' ) {
-                element = editor.document.createElement( 'abbr' );
-                this.insertMode = true;
+				//if the element already exists when the user opens the dialog
+				//find the ascendant of the element where the cursor was that
+				//has a class == 'pullquote' and set it as the element
+				element = element.getAscendant(function(el){
+					console.log(el.$.nodeName);
+					if(el.$.nodeName == 'BODY'){
+						console.log('THIS IS THE BODY');
+						return el;
+					}
+					return el.hasClass('pullquote') == true;
+				}, true);
+
+			console.log(element);
+            if ( !element || element.hasClass('pullquote') != true ) {
+                //CHANGE OLD
+            	//element = editor.document.createElement( 'abbr' );
+				console.log(this);
+
+				element = setUpPullquoteElement(editor);
+
+				this.insertMode = true;
             }
             else
                 this.insertMode = false;
@@ -84,13 +104,52 @@ CKEDITOR.dialog.add( 'divepullquoteDialog', function ( editor ) {
 
 		onOk: function() {
 			var dialog = this;
-			var	abbr = dialog.element;
+			console.log(dialog);
+			var	pullquote = dialog.element;
+			console.log(pullquote)
 
-			dialog.commitContent( abbr );
+			dialog.commitContent( pullquote );
 
 			if ( dialog.insertMode )
-				editor.insertElement( abbr );
+				editor.insertElement( pullquote );
 		}
 
     };
 });
+
+
+function setUpPullquoteElement(editor){
+	// var pullquoteElement = editor.document.createElement('div');
+	// pullquoteElement.className = 'pullquote';
+	// console.log(pullquoteElement);
+    var element = new CKEDITOR.dom.element('div').addClass('pullquote');
+    var quote = new CKEDITOR.dom.element('p').addClass('quote').appendTo(element);
+    var clearfix = new CKEDITOR.dom.element('div').addClass('clearfix').appendTo(element);
+
+    var headshot = new CKEDITOR.dom.element('div').addClass('headshot')//.appendTo(element);
+    var img = new CKEDITOR.dom.element('img').appendTo(headshot);
+    headshot.appendTo(element);
+
+    var speakerDetails = new CKEDITOR.dom.element('div').addClass('speaker-details');
+    var name = new CKEDITOR.dom.element('p').addClass('name').appendTo(speakerDetails);
+    var title = new CKEDITOR.dom.element('p').addClass('title').appendTo(speakerDetails);
+    speakerDetails.appendTo(element);
+
+    console.log(element);
+}
+
+
+function getPullquoteChild()
+
+
+// <div class="pullquote">
+//   <p class="quote">“By 2023, whites will comprise less than half of the U.S. population under age 30.”</p>
+//   <div class="clearfix"></div>
+//   <div class="headshot">
+//     <img src="http://www.educationdive.com/user_media/diveimage/william_frey_znzDrca.jpg">
+//   </div>
+//   <div class="speaker-details">
+//     <p class="name">William Frey</p>
+//     <p class="title">Author of “Diversity Explosion” and demographer at Brookings Institution's Metropolitan Policy Program</p>
+//   </div>
+// </div>
--- ckeditor/static/ckeditor/ckeditor/plugins/divepullquote/plugin.js
+++ ckeditor/static/ckeditor/ckeditor/plugins/divepullquote/plugin.js
@@ -1,20 +1,20 @@
 "use strict";
 
 CKEDITOR.plugins.add( 'divepullquote', {
-    icons: 'divepullquote',
+    icons: 'pullquote',
     init: function( editor ) {
         console.log(this);
         
-        editor.addCommand( 'doDivepullquote', {
-            exec: function( editor ) {
-                //var now = new Date();
-                
-                // var test = '<div class="slider-center"><div><img src="https://placebear.com/200/500" /></div><div><img src="https://placebear.com/300/300" /></div><div><img src="https://placebear.com/500/200" /></div></div>';
-                //
-                // //editor.insertHtml( 'The current date and time is: <em>' + now.toString() + '</em>' );
-                // editor.insertHtml( test );
-            }
-        });
+        // editor.addCommand( 'doDivepullquote', {
+        //     exec: function( editor ) {
+        //         //var now = new Date();
+        //
+        //         // var test = '<div class="slider-center"><div><img src="https://placebear.com/200/500" /></div><div><img src="https://placebear.com/300/300" /></div><div><img src="https://placebear.com/500/200" /></div></div>';
+        //         //
+        //         // //editor.insertHtml( 'The current date and time is: <em>' + now.toString() + '</em>' );
+        //         // editor.insertHtml( test );
+        //     }
+        // });
 
         //ties a button press editor command to a dialog command
         editor.addCommand('doDivepullquote', new CKEDITOR.dialogCommand( 'divepullquoteDialog' ) );
@@ -22,32 +22,32 @@ CKEDITOR.plugins.add( 'divepullquote', {
         //adds a dialog to the plugin
         CKEDITOR.dialog.add( 'divepullquoteDialog', this.path + 'dialogs/divepullquote.js' );
 
-        editor.ui.addButton( 'Pull Quote', {
+        editor.ui.addButton( 'PullQuote', {
             label: 'Insert Pull Quote',
-            command: 'doDivepullquote'
+            command: 'doDivepullquote',
+            toolbar: 'basic'
         });
 
 
 
         //to edit the element that's already be put in ('abbr' in the tutorial case')
-        /*
+
         if ( editor.contextMenu ) {
-            editor.addMenuGroup( 'sliderGroup' );
-            editor.addMenuItem( 'sliderItem', {
+            editor.addMenuGroup( 'divepullquoteGroup' );
+            editor.addMenuItem( 'divepullquoteItem', {
                 label: 'Edit Abbreviation',
-                icon: this.path + 'icons/slider.png',
-                command: 'doSlider',
-                group: 'sliderGroup'
+                icon: this.path + 'icons/pullquote.png',
+                command: 'doDivepullquote',
+                group: 'divepullquoteGroup'
             });
 
-            console.log( editor.contextMenu );
 
             editor.contextMenu.addListener( function( element ) {
                 if ( element.getAscendant( 'abbr', true ) ) {
-                    return { sliderItem: CKEDITOR.TRISTATE_OFF };
+                    return { divepullquoteItem: CKEDITOR.TRISTATE_OFF };
                 }
             });
         }
-        */
+
     }
 });
